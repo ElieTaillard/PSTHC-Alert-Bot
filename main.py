be7ff9ef434cd3272f53ac0928e8c56fea7a60ff
Elie Taillard <taillardelie@gmail.com>
2023-10-26 16:22:31 +0200

    refacto

--- main.py
+++ main.py
@@ -1,41 +1,19 @@
 import json
-import logging
-import time
-from collections import deque
 
 import discord
-import feedparser
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 import settings
-
-last_entry_id = None
-rss_url = "https://www.psthc.fr/flux.xml"
+from psthc_bot import PsthcBot
 
 
 def run():
-    global last_entry_id
-    feed = feedparser.parse(rss_url)
-    if len(feed.entries) > 0:
-        last_entry_id = feed.entries[0].id
-
-    intents = discord.Intents.default()
-    intents.message_content = True
-    bot = commands.Bot(
-        command_prefix=".psthc", status=discord.Status.online, intents=intents
+    bot = PsthcBot(
+        intents=discord.Intents.default(),
+        command_prefix=".psthc",
+        status=discord.Status.online,
+        rss_url="https://www.psthc.fr/flux.xml",
     )
-
-    @bot.event
-    async def on_ready():
-        logging.info(f"Utilisateur connecté : {bot.user} (ID : {bot.user.id})")
-
-        logging.info("Synchronisation des commandes...")
-        await bot.tree.sync()
-
-        if not check_rss.is_running():
-            logging.info("Lancement de la Task 'check_rss' ...")
-            check_rss.start()
-            logging.info("La Task 'check_rss' est lancée")
 
     @bot.tree.command(
         name="setchannel",
@@ -60,25 +38,6 @@ def run():
             await interaction.response.send_message(
                 f"Une erreur est survenue, veuillez réessayer...", ephemeral=True
             )
-
-    @tasks.loop(seconds=1)
-    async def check_rss():
-        global last_entry_id
-        feed = feedparser.parse(rss_url)
-
-        if len(feed.entries) > 0:
-            entry = feed.entries[0]
-
-            if entry.id != last_entry_id:
-                logging.info(f"Nouvel item détecté : {entry.title}")
-                last_entry_id = entry.id
-
-                with open("channel.json", "r") as jsonFile:
-                    data = json.load(jsonFile)
-
-                channel = bot.get_channel(data["channel_id"])
-
-                await channel.send(f"Nouvel item détecté : {entry.title}")
 
     bot.run(settings.TOKEN, root_logger=True)
 
---
+++ psthc_bot.py
@@ -0,0 +1,54 @@
+import asyncio
+import json
+import logging
+
+import feedparser
+from discord.ext import commands
+
+
+class PsthcBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.rss_url = kwargs.get("rss_url", None)
+        self.last_entry_id = None
+
+        feed = feedparser.parse(self.rss_url)
+        if len(feed.entries) > 0:
+            self.last_entry_id = feed.entries[0].id
+
+    async def setup_hook(self) -> None:
+        logging.info("Création de la Task check RSS en arrière plan")
+        self.bg_task = self.loop.create_task(self.check_rss())
+
+    async def on_ready(self):
+        logging.info(f"Utilisateur connecté : {self.user} (ID : {self.user.id})")
+        logging.info("Synchronisation des commandes...")
+        await self.tree.sync()
+        logging.info("Commandes synchronisée")
+
+    async def check_rss(self):
+        await self.wait_until_ready()
+
+        logging.info("Check du flux RSS...")
+
+        while not self.is_closed():
+            feed = feedparser.parse(self.rss_url)
+
+            if len(feed.entries) > 0:
+                entry = feed.entries[0]
+
+                if entry.id != self.last_entry_id:
+                    logging.info(f"Nouvel item détecté : {entry.title}")
+                    self.last_entry_id = entry.id
+
+                    with open("channel.json", "r") as jsonFile:
+                        data = json.load(jsonFile)
+
+                    channel = self.get_channel(data["channel_id"])
+
+                    await channel.send(f"Nouvel item détecté : {entry.title}")
+
+            await asyncio.sleep(1)
+
+        logging.warning("La connexion WebSocket s'est fermée")
